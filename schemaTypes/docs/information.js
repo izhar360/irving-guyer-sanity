fceeb2e1b3e2aca5ae3f0a70d41990060b9a3f66
Muhammad Izhar <79567009+izhar360@users.noreply.github.com>
2024-11-30 17:41:57 +0500

    wrtiting schema updated

--- schemaTypes/docs/information.js
+++ schemaTypes/docs/information.js
@@ -51,12 +51,66 @@ export default {
     },
 
     {
-        name: 'writing',
-        title: 'Writing',
-        type: 'string',
-        group: 'writing',
-     
-      },
+      name: 'writing',
+      title: 'Writing',
+      type: 'array',
+      group: 'writing',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'title',
+              title: 'Title',
+              type: 'string',
+            },
+            {
+              name: 'description',
+              title: 'Description',
+              type: 'array',
+              of: [
+                {
+                  type: 'block',
+                  styles: [
+                    { title: 'Normal', value: 'normal' },
+                    { title: 'Heading 1', value: 'h1' },
+                    { title: 'Heading 2', value: 'h2' },
+                    { title: 'Heading 3', value: 'h3' },
+                    { title: 'Quote', value: 'blockquote' },
+                  ],
+                  lists: [
+                    { title: 'Bullet', value: 'bullet' },
+                    { title: 'Numbered', value: 'number' },
+                  ],
+                  marks: {
+                    decorators: [
+                      { title: 'Bold', value: 'strong' },
+                      { title: 'Italic', value: 'em' },
+                      { title: 'Underline', value: 'underline' },
+                      { title: 'Strike-through', value: 'strike-through' },
+                    ],
+                    annotations: [
+                      {
+                        name: 'link',
+                        type: 'object',
+                        title: 'URL',
+                        fields: [
+                          {
+                            name: 'href',
+                            type: 'url',
+                            title: 'URL',
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }    
   ],
   preview: {
     select: {
